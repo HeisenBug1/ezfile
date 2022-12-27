09f14927ac1d8344e3fb173b82f54f11593e2d7d
M. R. Hossain <rez182@gmail.com>
2022-12-26 23:55:23 -0500

    ADDED: print only & symbolic file linking

--- ezfile.py
+++ ezfile.py
@@ -8,151 +8,183 @@ mime = magic.Magic(mime=True)
 
 # convert a path to unix path
 def unix_path(path):
-    if isinstance(path, str):
-        return path.replace(" ", "\\ ")
+	if isinstance(path, str):
+		return path.replace(" ", "\\ ")
 
 # adds the '/' at the end of a directory
 def get_dir_path(item):
-    if os.path.isdir(item):
-        if item[-1] != '/':
-            return item+'/'
-    return item
+	if os.path.isdir(item):
+		if item[-1] != '/':
+			return item+'/'
+	return item
 
 # remove current linked files
 def rem_all_links(path):
-    if len(os.listdir(path)) > 0:
-        os.system('rm '+unix_path(path)+'*')
+	if len(os.listdir(path)) > 0:
+		os.system('rm '+unix_path(path)+'*')
 
 # find files recursively based on type
-def find(src, dst, counter=False):
-    file_list = []
-    dir_list = []
+def find(src="", dst="", type="", counter=False):
 
-    if counter == True:
-        counter = 0
-    else:
-        print("Counter set to: ["+str(counter) +
-              "] (Required Boolean Value) Defaulting to False")
-        counter = -1
+	# check required parameters
+	if None in [src, dst, type]:
+		print("Missing required parameters in find(src,dst,type)")
+		sys.exit(4)
 
-    while True:
-        if src != dst:
-            dir = os.listdir(src)
-            new_dir = True
+	file_list = []
+	dir_list = []
 
-            for item in dir:
-                item_path = src+item
+	if counter == True:
+		counter = 0
+	else:
+		counter = -1
 
-                # if current item is a file
-                if os.path.isfile(item_path):
-                    file_type = mime.from_file(item_path)
+	while True:
+		if src != dst:
+			dir = os.listdir(src)
+			new_dir = True
 
-                    # if file is of required type
-                    if file_type.find('audio') != -1:
-                        # append counter to filename if option was enabled
-                        if counter >= 0:
-                            if new_dir:
-                                counter += 1
-                                new_dir = False
-                            file_list.append(dst+str(counter)+" "+item)
-                        else:
-                            file_list.append(dst+item)
+			for item in dir:
+				item_path = src+item
 
-                # if current item is a sub directory
-                else:
-                    dir_list.append(get_dir_path(item_path))
+				# if current item is a file
+				if os.path.isfile(item_path):
+					file_type = mime.from_file(item_path)
 
-        if len(dir_list) == 0:
-            break
-        else:
-            src = dir_list.pop()
+					# if file is of required type
+					if file_type.find(type) != -1:
+						# append counter to filename if option was enabled
+						if counter >= 0:
+							if new_dir:
+								counter += 1
+								new_dir = False
+							file_list.append((item_path, dst+str(counter)+" "+item))
+						else:
+							file_list.append((item_path, dst+item))
 
-    return file_list
+				# if current item is a sub directory
+				else:
+					dir_list.append(get_dir_path(item_path))
+
+		if len(dir_list) == 0:
+			break
+		else:
+			src = dir_list.pop()
+
+	return file_list
 
 # read user args
 def read_args():
 
-    app_desc = """A recursive file manupulation app based on file MIME type.
+	app_desc = """A recursive file manupulation app based on file MIME type.
 	Similar to a unix wildcard operation, but not depended on file extention.
 	Example, if you want to copy/move all music files from one location to another,
 		but you have mp3s and aac. In unix you would have to know the extention and use wildcard with
 		known extension like '*.mp3' or '*.acc'. This app will instead analyze the MIME of the file
 		and determine it's type even if it has no extension."""
 
-    parser = argparse.ArgumentParser(description=app_desc)
+	parser = argparse.ArgumentParser(description=app_desc)
 
-    parser.add_argument("-V", "--version", help="show program version", action="store_true")
-    parser.add_argument('--copy', help='Copy files', action="store_true")
-    parser.add_argument('--move', help='Move files', action="store_true")
-    parser.add_argument('--link', help='Symbolic link files', action="store_true")
-    parser.add_argument("-s", "--source", help="The source directory to search in", required=True)
-    parser.add_argument("-d", "--destination", help="The destination directory to [copy|move|link] files to", required=True)
-    parser.add_argument("-t", "--type", help="File type to search: [audio|video|python|etc]", required=True)
-    parser.add_argument('--serialize', '--counter', help='Appends a counter suffix to each file per sub-directory. (Helps with grouping)', default=False, action="store_true")
+	parser.add_argument("-V", "--version", help="show program version", action="store_true")
+	parser.add_argument('-c', '--copy', help='Copy files', action="store_true")
+	parser.add_argument('-m', '--move', help='Move files', action="store_true")
+	parser.add_argument('-l', '--link', help='Symbolic link files', action="store_true")
+	parser.add_argument('-p', '--print', help='Print to screen', action='store_true')
+	parser.add_argument("-s", "--source", help="The source directory to search in", required=True)
+	parser.add_argument("-d", "--destination", help="The destination directory to [copy|move|link] files to")
+	parser.add_argument("-t", "--type", help="File type to search: [audio|video|python|etc]", required=True)
+	parser.add_argument('--serialize', '--counter', help='Appends a counter suffix to each file per sub-directory. (Helps with grouping)', default=False, action="store_true")
 
-    args = parser.parse_args()
+	args = parser.parse_args()
 
-    if args.version:
-        print("exfile version: 0.1")
-        sys.exit(0)
+	if args.version:
+		print("exfile version: 0.1")
+		sys.exit(0)
 
-    # verify file operation choice
-    op_choices = []
-    if args.copy:
-        op_choices.append('cp')
-    if args.move:
-        op_choices.append('mv')
-    if args.link:
-        op_choices.append('ln -s')
-    if len(op_choices) != 1:
-        print("Error: 0 or > 1 file operation provided. Choose between [copy|move|link]")
-        sys.exit(3)
+	# verify file operation choice
+	op_choices = []
+	if args.copy:
+		op_choices.append('cp')
+	if args.move:
+		op_choices.append('mv')
+	if args.link:
+		op_choices.append('link')
+	if args.print:
+		op_choices.append('print')
+	if len(op_choices) != 1:
+		print("Error: 0 or > 1 file operation provided. Choose between [copy|move|link|print]")
+		sys.exit(3)
 
-    # verify srouce directory exists
-    if not os.path.isdir(args.source):
-        print("Source: ["+args.source+"] is not a valid directory")
-        sys.exit(1)
+	# verify srouce directory exists
+	if not os.path.isdir(get_dir_path(os.path.abspath(args.source))):
+		print(f"Source: [{get_dir_path(os.path.abspath(args.source))}] is not a valid directory")
+		sys.exit(1)
 
-    # verify destination directory exists (else create it)
-    if not os.path.isdir(args.destination):
-        print(f"Destination: [{args.destination}] is not a valid directory")
-        response = input("Do you want to create it? ").lower()
-        if response == 'y' or response == 'yes':
-            try:
-                os.makedirs(get_dir_path(args.destination))
-            except FileExistsError:
-                print("Error: Cannot create directory since a file already exists with that name")
-                sys.exit(2)
-        else:
-            sys.exit(0)
+	# store values to return
+	arg_dict = {
+		'src': get_dir_path(os.path.abspath(args.source)),
+		'type': args.type,
+		'counter': args.serialize,
+		'op': op_choices.pop(),
+		'dst': ""
+	}
 
-    arg_dict = {
-        'src': args.source,
-        'dst': args.destination,
-        'type': args.type,
-        'counter': args.serialize,
-        'op': op_choices.pop()
-    }
+	# if print only, return
+	if arg_dict['op'] == 'print':
+		return arg_dict
 
-    return arg_dict
+	# ELSE verify destination directory exists (else create it)
+	elif arg_dict['op'] != 'print' and args.destination:
+		dst = get_dir_path(os.path.abspath(args.destination))
+		if not os.path.isdir(dst):
+			print(f"Destination: [{dst}] is not a valid directory")
+			response = input("Do you want to create it? ").lower()
+			if response == 'y' or response == 'yes':
+				try:
+					os.makedirs(dst)
+					arg_dict['dst'] = dst
+				except FileExistsError:
+					print("Error: Cannot create directory since a file already exists with that name")
+					sys.exit(2)
+			else:
+				print("Quitting...")
+				sys.exit(0)
+		else:
+			arg_dict['dst'] = dst
+	else:
+		print("Missing destination path. [(-d | --destination) /path/to/dir/]")
+		sys.exit(5)
+	
+	return arg_dict
 
 # main
 def main():
 
-    # read user arguments
-    args = read_args()
-    print(args)
+	# read user arguments
+	args = read_args()
+	src = args['src']
+	dst = args['dst']
+	type = args['type']
+	file_op = args['op']
+	serialize = args['counter']
 
-    # dst = "./test/"
-    # src = "./test/"
+	# rem_all_links(dst)
+	
+	file_list = find(src, dst, type, serialize)
 
-    # rem_all_links(dst)
-    # src = os.path.abspath(src)
-    # dst = os.path.abspath(dst)
-    # file_list = find(get_dir_path(src), get_dir_path(dst), True)
-    # for file in file_list:
-    # 	print(file)
+	print(f"""Found: [{len(file_list)}] items in [{args['src']}]
+	Performing [{args['op']}] task on them to [{args['dst']}]""")
+
+	for file_src, file_dst in file_list:
+		if file_op == 'print':
+			print(file_src)
+		if file_op == 'link':
+			try:
+				os.symlink(file_src, file_dst)
+			except(FileExistsError):
+				# file exists
+				continue
 
 
 if __name__ == "__main__":
-    main()
+	main()
